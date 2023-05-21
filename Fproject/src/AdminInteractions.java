cb52dd9adbf2e5cb12ca468f31e2370be4cfde22
Muhammed Ashraf <mohamed8ashraf2@gmail.com>
2023-05-21 04:02:43 +0300

    test resolved

--- Fproject/src/AdminInteractions.java
+++ Fproject/src/AdminInteractions.java
@@ -1,5 +1,3 @@
-
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -14,6 +12,8 @@ import javax.swing.JOptionPane;
 
 public class AdminInteractions extends javax.swing.JFrame {
 
+    public boolean Exist;
+
     public AdminInteractions() {
 
         initComponents();
@@ -26,12 +26,8 @@ public class AdminInteractions extends javax.swing.JFrame {
 
         jPanel2 = new javax.swing.JPanel();
         jButton1 = new javax.swing.JButton();
-
-        jTextField1 = new javax.swing.JTextField();
-        jComboBox1 = new javax.swing.JComboBox<>();
-
-        jButton8 = new javax.swing.JButton();
-        jButton7 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -44,33 +40,30 @@ public class AdminInteractions extends javax.swing.JFrame {
         jButton1.setFont(new java.awt.Font("Goudy Old Style", 1, 18)); // NOI18N
         jButton1.setForeground(new java.awt.Color(255, 255, 255));
         jButton1.setText("Edit Account");
+        jButton1.setActionCommand("Delete Account");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
             }
         });
 
-        jTextField1.addActionListener(new java.awt.event.ActionListener() {
-
-        jButton8.setBackground(new java.awt.Color(0, 124, 169));
-        jButton8.setFont(new java.awt.Font("Goudy Old Style", 1, 18)); // NOI18N
-        jButton8.setForeground(new java.awt.Color(255, 255, 255));
-        jButton8.setText("Delete account");
-        jButton8.addActionListener(new java.awt.event.ActionListener() {
+        jButton2.setBackground(new java.awt.Color(0, 124, 169));
+        jButton2.setFont(new java.awt.Font("Goudy Old Style", 1, 18)); // NOI18N
+        jButton2.setForeground(new java.awt.Color(255, 255, 255));
+        jButton2.setText("Delete account");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton8ActionPerformed(evt);
+                jButton2ActionPerformed(evt);
             }
         });
 
-        jButton7.setBackground(new java.awt.Color(0, 124, 169));
-        jButton7.setFont(new java.awt.Font("Goudy Old Style", 1, 18)); // NOI18N
-        jButton7.setForeground(new java.awt.Color(255, 255, 255));
-        jButton7.setText("Search Phone");
-        jButton7.addActionListener(new java.awt.event.ActionListener() {
+        jButton3.setBackground(new java.awt.Color(0, 124, 169));
+        jButton3.setFont(new java.awt.Font("Goudy Old Style", 1, 18)); // NOI18N
+        jButton3.setForeground(new java.awt.Color(255, 255, 255));
+        jButton3.setText("Search Phone");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jComboBox1ActionPerformed(evt);
-
-                jButton7ActionPerformed(evt);
+                jButton3ActionPerformed(evt);
             }
         });
 
@@ -78,50 +71,26 @@ public class AdminInteractions extends javax.swing.JFrame {
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGap(272, 272, 272)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 187, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(183, 183, 183)
-                .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 187, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(241, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 187, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(424, 424, 424))
-
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
                 .addContainerGap(298, Short.MAX_VALUE)
-                .addComponent(jButton7, javax.swing.GroupLayout.PREFERRED_SIZE, 149, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 149, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(124, 124, 124)
                 .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 149, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(350, 350, 350))
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addGap(435, 435, 435)
-                .addComponent(jButton8, javax.swing.GroupLayout.PREFERRED_SIZE, 149, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 149, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGap(176, 176, 176)
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(84, 84, 84)
-                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(254, Short.MAX_VALUE))
-        );
-
-        getContentPane().add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1070, 600));
-
-        getContentPane().add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 60, 1070, 600));
-
                 .addGap(125, 125, 125)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 116, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton7, javax.swing.GroupLayout.PREFERRED_SIZE, 116, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 116, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(77, 77, 77)
-                .addComponent(jButton8, javax.swing.GroupLayout.PREFERRED_SIZE, 116, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 116, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(166, Short.MAX_VALUE))
         );
 
@@ -137,83 +106,7 @@ public class AdminInteractions extends javax.swing.JFrame {
         obj.setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
 
-    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField1ActionPerformed
-
-    private void jComboBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox1ActionPerformed
-       String selectedItem = (String) jComboBox1.getSelectedItem();
-    if (selectedItem.equals("Sum")) {
-           if (selectedItem.equals("Sum")) {
-        try {    
-    Connection conn = DriverManager.getConnection("jdbc:derby://localhost:1527/E-Wallet", "root", "root");
-        String sql = "SELECT SUM(DOUBLE(BALANCE)) FROM WALLET";
-        Statement pstat = conn.createStatement();
-        ResultSet rs = pstat.executeQuery(sql);
-        if (rs.next()) {      
-            double totalBalance = rs.getDouble(1);
-            jTextField1.setText("Sum of balance = " + "(" + totalBalance + ")");
-        }
-            conn.close();
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-    }}
-    else if (selectedItem.equals("Average")) {
-           if (selectedItem.equals("Average")) {
-        try {    
-    Connection conn = DriverManager.getConnection("jdbc:derby://localhost:1527/E-Wallet", "root", "root");
-        String sql = "SELECT AVG(DOUBLE(BALANCE)) FROM WALLET";
-        Statement pstat = conn.createStatement();
-        ResultSet rs = pstat.executeQuery(sql);
-        if (rs.next()) {      
-            double averageBalance = rs.getDouble(1);
-            jTextField1.setText("Average of balance= " + "(" + averageBalance + ")");
-        }
-            conn.close();
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-    }}
-    else if (selectedItem.equals("Maximum")) {
-           if (selectedItem.equals("Maximum")) {
-        try {    
-    Connection conn = DriverManager.getConnection("jdbc:derby://localhost:1527/E-Wallet", "root", "root");
-        String sql = "SELECT MAX(DOUBLE(BALANCE)) FROM WALLET";
-        Statement pstat = conn.createStatement();
-        ResultSet rs = pstat.executeQuery(sql);
-        if (rs.next()) {      
-            double MaximumBalance = rs.getDouble(1);
-            jTextField1.setText("Maximum balance= " + "(" + MaximumBalance + ")");
-        }
-            conn.close();
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-    }}
-    else if (selectedItem.equals("Minimum")) {
-           if (selectedItem.equals("Minimum")) {
-        try {    
-    Connection conn = DriverManager.getConnection("jdbc:derby://localhost:1527/E-Wallet", "root", "root");
-        String sql = "SELECT MIN(DOUBLE(BALANCE)) FROM WALLET";
-        Statement pstat = conn.createStatement();
-        ResultSet rs = pstat.executeQuery(sql);
-        if (rs.next()) {      
-            double MinimumBalance = rs.getDouble(1);
-            jTextField1.setText("Minimum balance= " + "(" + MinimumBalance + ")");
-        }
-            conn.close();
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-    }}
-    }//GEN-LAST:event_jComboBox1ActionPerformed
-
-    /**
-     * @param args the command line arguments
-     */
-
-    private void jButton8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton8ActionPerformed
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         String phone = JOptionPane.showInputDialog("Enter Phone Number:");
         try {
             Connection conn = DriverManager.getConnection("jdbc:derby://localhost:1527/E-Wallet", "root", "root");
@@ -229,16 +122,16 @@ public class AdminInteractions extends javax.swing.JFrame {
         } catch (SQLException ex) {
             Logger.getLogger(AdminInteractions.class.getName()).log(Level.SEVERE, null, ex);
         }
-    }//GEN-LAST:event_jButton8ActionPerformed
+    }//GEN-LAST:event_jButton2ActionPerformed
 
-    private void jButton7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton7ActionPerformed
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         String phone = JOptionPane.showInputDialog("Enter Phone Number:");
         if (searchPhone(phone)) {
             JOptionPane.showMessageDialog(this, "phone is exist");
         } else {
             JOptionPane.showMessageDialog(this, "phone is not exist");
         }
-    }//GEN-LAST:event_jButton7ActionPerformed
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     public boolean searchPhone(String phone) {
         Connection conn;
@@ -282,6 +175,7 @@ public class AdminInteractions extends javax.swing.JFrame {
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
             public void run() {
                 new AdminInteractions().setVisible(true);
             }
@@ -290,10 +184,8 @@ public class AdminInteractions extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
-    private javax.swing.JComboBox<String> jComboBox1;
-
-    private javax.swing.JButton jButton7;
-    private javax.swing.JButton jButton8;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel2;
     // End of variables declaration//GEN-END:variables
